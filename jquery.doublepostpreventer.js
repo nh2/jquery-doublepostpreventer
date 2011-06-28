958a3c3407ae6b2fe43070091cb26c52f245c6b6
Niklas Hambüchen <nh2@deditus.de>
2011-06-28 01:23:08 +0100

    Actual plugin and use

---
+++ jquery.doublepostpreventer.js
@@ -0,0 +1,34 @@
+/*
+ * jQuery DoublepostPreventer 1.0
+ * - Disabling submit buttons after submit
+ *
+ * Copyright (c) 2011 Niklas Hambuechen (nh2.me)
+ * Licensed under the MIT license.
+ *
+ * $Date: 2011/06/28$
+ */
+
+/*
+ * Usage example:
+ * $("form").doublepostpreventer();
+ */
+
+(function($){
+
+	$.fn.doublepostpreventer = function(){
+
+		// Disable submit buttons after clicking to prevent double submissions
+		// Disable them only if no other submit event handler is registered
+
+		return this.find('form').andSelf().filter('form').submit(function() {
+			var $form = $(this);
+			var all_submit_handlers = $form.data('events').submit;
+			// check if only this event handler we're in now is registered <-> only one handler is registered
+			if (all_submit_handlers !== undefined && all_submit_handlers.length == 1) {
+				$form.find('input[type=submit]').attr('disabled', 'disabled');
+				console.log("disabling");
+			}
+		});
+
+	};
+})(jQuery);
